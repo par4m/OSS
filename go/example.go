66d8ba90d7abd77176efbae716a3222a4a2888c7
par4m <param2k3@gmail.com>
2024-12-11 00:30:48 +0530

    mac

---
+++ go/example.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+
+	type person struct {
+		Name string
+		Age  int
+	}
+	p := person{Name: "Param", Age: 21}
+
+}
---
+++ go/types.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func T() {
+	var i int = 42
+	var f float64 = float64(i)
+	fmt.Println(f)
+}
